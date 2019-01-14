a795390f0f54b6a3be29ba5e2ca32a15d7049129
Henrique Lopes <riquellopes@gmail.com>
2019-01-13 22:05:55 -0200

    init fizzbuzz dotnet

---
+++ fizzBuzz/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace fizzBuzz
+{
+    public class FizzBuzz
+    {
+        private int numeric;
+
+        public int Numeric { get => numeric; set => numeric = value; }
+
+        public string GetResult()
+        {
+            if(numeric > 0 && numeric % 3 == 0)
+            {
+                return "fizz";
+            }
+
+            if (numeric > 0 && numeric % 5 == 0)
+            {
+                return "buzz";
+            }
+
+            return numeric.ToString();
+        }
+    }
+}
---
+++ fizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,63 @@
+using System;
+using Xunit;
+
+namespace fizzBuzz
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestFizzBuzzGet0()
+        {
+            FizzBuzz f = new FizzBuzz
+            {
+                Numeric = 0
+            };
+
+            Assert.Equal("0", f.GetResult());
+        }
+
+        [Fact]
+        public void TestFizzBuzz1()
+        {
+            FizzBuzz f = new FizzBuzz
+            {
+                Numeric = 1
+            };
+
+            Assert.Equal("1", f.GetResult());
+        }
+
+        [Fact]
+        public void TestFizzBuzz3()
+        {
+            FizzBuzz f = new FizzBuzz
+            {
+                Numeric = 3
+            };
+
+            Assert.Equal("fizz", f.GetResult());
+        }
+
+        [Fact]
+        public void TestFizzBuzz9()
+        {
+            FizzBuzz f = new FizzBuzz
+            {
+                Numeric = 9
+            };
+
+            Assert.Equal("fizz", f.GetResult());
+        }
+
+        [Fact]
+        public void TestFizzBuzz5()
+        {
+            FizzBuzz f = new FizzBuzz
+            {
+                Numeric = 5
+            };
+
+            Assert.Equal("buzz", f.GetResult());
+        }
+    }
+}
